08d3d3514877ccc5ba6f37dcf3a323f33dbdc97a
TranManhSon <83875162+TranManhSon@users.noreply.github.com>
2023-05-03 23:58:08 +0700

    add create sensor

--- src/main/java/com/example/coap_demo/client/CoapPostSensorProcess.java
+++ src/main/java/com/example/coap_demo/client/CoapPostSensorProcess.java
@@ -10,12 +10,29 @@ import java.io.IOException;
 public class CoapPostSensorProcess {
     public static final String SENSOR_LIST_URL = "coap://localhost:5683/sensor/"; // Địa chỉ URI của server CoAP
 
-    private final static Integer NUM_SENSORS = 1;
+    private static Integer NUM_ID_SENSORS = 0;
 
-    public static void main(String[] args) throws ConnectorException, IOException {
-        // Khởi tạo và chạy các luồng Sensor
-        for (int i = 0; i < NUM_SENSORS; i++) {
+//    public static void main(String[] args) throws ConnectorException, IOException {
+//        // Khởi tạo và chạy các luồng Sensor
+//        for (int i = 0; i < NUM_SENSORS; i++) {
+//            Sensor sensor = new Sensor("Sensor00" + (i + 1), 0.0); // Thay đổi giá trị cho từng Sensor tại đây
+//            // Tạo và khởi chạy luồng Sensor
+//            Thread sensorThread = new Thread(new SensorThread(sensor));
+//            sensorThread.start();
+//        }
+//    }
+
+    public void handleCreateOneSensor() {
+        Sensor sensor = new Sensor("Sensor00" + (NUM_ID_SENSORS + 1), 0.0);
+        NUM_ID_SENSORS++;
+        Thread sensorThread = new Thread(new SensorThread(sensor));
+        sensorThread.start();
+    }
+
+    public void handleCreateManySensor( int numberSensor) {
+        for (int i = 0; i < numberSensor; i++) {
             Sensor sensor = new Sensor("Sensor00" + (i + 1), 0.0); // Thay đổi giá trị cho từng Sensor tại đây
+            NUM_ID_SENSORS++;
             // Tạo và khởi chạy luồng Sensor
             Thread sensorThread = new Thread(new SensorThread(sensor));
             sensorThread.start();
---
+++ src/main/java/com/example/coap_demo/server/resource/AddSensorResource.java
@@ -0,0 +1,71 @@
+package com.example.coap_demo.server.resource;
+
+import com.example.coap_demo.client.CoapPostSensorProcess;
+import com.example.coap_demo.model.Sensor;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.Response;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.json.JSONObject;
+
+import java.security.InvalidParameterException;
+
+public class AddSensorResource extends CoapResource {
+    private static Integer NUM_SENSORS_CURRENTLY = 0;
+
+    public AddSensorResource(String name) {
+        super(name);
+
+        setObservable(true); // enable observing
+        setObserveType(CoAP.Type.CON); // configure the notification type to CONs
+
+        getAttributes().setTitle("Sensor create Observable Resource");
+        getAttributes().setObservable(); // mark observable in the Link-Format
+    }
+
+    @Override
+    public void handleGET(CoapExchange exchange) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("Number sensor currently", NUM_SENSORS_CURRENTLY);
+
+        // Trả về chuỗi JSON làm nội dung của phản hồi
+        exchange.respond(CoAP.ResponseCode.CONTENT, jsonObject.toString(), MediaTypeRegistry.APPLICATION_JSON);
+    }
+
+    @Override
+    public void handlePUT(CoapExchange exchange) {
+
+        System.out.println("test create ");
+
+        // Nhận payload từ yêu cầu CoAP
+        byte[] payload = exchange.getRequestPayload();
+
+        // Chuyển đổi chuỗi payload thành đối tượng JSONObject
+        String payloadStr = new String(payload);
+
+        JSONObject jsonObject = new JSONObject(payloadStr);
+
+        // Lấy giá trị từ đối tượng JSONObject
+        Integer numberNewSensor = jsonObject.getInt("number");
+
+        // cap nhat number sensor currently
+        NUM_SENSORS_CURRENTLY += numberNewSensor;
+
+//        CoapPostSensorProcess coapPostSensorProcess = new CoapPostSensorProcess();
+//        if (numberNewSensor > 1) {
+//            coapPostSensorProcess.handleCreateManySensor(numberNewSensor);
+//        } else if ( numberNewSensor == 1) {
+//            coapPostSensorProcess.handleCreateOneSensor();
+//        } else {
+//            throw new InvalidParameterException("Number sensor > 0");
+//        }
+
+        Response response = new Response(CoAP.ResponseCode.CHANGED);
+        response.setPayload("Đã nhận và xử lý payload thành công".getBytes());
+        exchange.respond(response);
+    }
+}
