78540e913fcefec9e9cb9a447a6f71ef3056f9f6
Mạnh Hùng <52119265+dolemanhhung@users.noreply.github.com>
2023-05-08 12:17:56 +0700

    double-check

--- src/main/java/com/example/coap_demo/client/SensorThread.java
+++ src/main/java/com/example/coap_demo/client/SensorThread.java
@@ -15,7 +15,7 @@ import java.util.TimerTask;
 
 import static com.example.coap_demo.client.CoapPostSensorProcess.SENSOR_LIST_URL;
 
-public class SensorThread implements Runnable{
+public class SensorThread implements Runnable {
     private Sensor sensor;
     private final static Logger logger = LoggerFactory.getLogger(SensorThread.class);
     private String SENSOR_URL;
@@ -28,7 +28,7 @@ public class SensorThread implements Runnable{
         this.sensorClient = new CoapClient(SENSOR_URL);
     }
 
-    private void getObserve(){
+    private void getObserve() {
         Request request = Request.newGet().setURI(this.SENSOR_URL).setObserve();
         request.setConfirmable(true);
 
@@ -50,7 +50,7 @@ public class SensorThread implements Runnable{
             public void onError() {
                 logger.error("OBSERVING FAILED");
             }
-        });
+        };
     }
 
     private void postToServer() {
