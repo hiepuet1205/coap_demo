ec6b2078794a8d27d9c7b94f60626d8f2e008a12
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-05-19 23:55:21 +0700

    add fe

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Sensors from './components/Sensors'
+
+function App() {
+    return (
+        <div className="App">
+            <Sensors></Sensors>
+        </div>
+    )
+}
+
+export default App
