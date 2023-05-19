49fcc5825ab9ede7c43e919584171a84b596528e
hiep <hiepvbhpnbk@gmail>
2023-05-19 23:27:34 +0700

    update

--- src/main/java/com/example/coap_demo/sensor/CoapUtils.java
+++ src/main/java/com/example/coap_demo/sensor/CoapUtils.java
@@ -18,29 +18,6 @@ public class CoapUtils {
     private static final String COAP_SERVER_URL = "coap://localhost:5683/performance";
     private static Integer NUM_ID_SENSORS = 0;
 
-    public void sendTimeStartClient() {
-        CoapClient client = new CoapClient(COAP_SERVER_URL);
-
-        Request request = new Request(CoAP.Code.POST);
-
-        request.setConfirmable(true);
-
-        // Tạo đối tượng JSON
-        JSONObject jsonObject = new JSONObject();
-
-        // Thiết lập thuộc tính
-        jsonObject.put("timeStart", new Date(System.currentTimeMillis()).toString());
-
-        request.setPayload(jsonObject.toString().getBytes());
-
-        CoapResponse coapResp = null;
-        try {
-            coapResp = client.advanced(request);
-        } catch (ConnectorException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void handleCreateOneSensor() {
         Sensor sensor = new Sensor("Sensor00" + (NUM_ID_SENSORS + 1), 0.0);
         NUM_ID_SENSORS++;
